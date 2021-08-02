77d36e28b7b6ccd10f7452a582084621ee1c6140
midrob <midrob@ttu.ee>
2021-08-02 16:56:02 +0300

    implement jpql

--- src/main/java/com/example/KnCare/repository/MemberRepository.java
+++ src/main/java/com/example/KnCare/repository/MemberRepository.java
@@ -3,8 +3,12 @@ package com.example.KnCare.repository;
 import com.example.KnCare.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecificationExecutor<Member> {
+    @Query("select u from Member u where u.employee.internationalName like %?1")
+    List<Member> findByFirstnameEndsWith(String internationalName);
 }
