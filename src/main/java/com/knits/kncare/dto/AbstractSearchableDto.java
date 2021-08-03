4c5317ef401e6d517e7e9663a465338b3ced792b
stefano.fiorenza.knits <stefano.fiorenza.knits@gmail.com>
2021-08-03 18:53:35 +0300

    moved some model classes to dto

---
+++ src/main/java/com/knits/kncare/dto/AbstractSearchableDto.java
@@ -0,0 +1,48 @@
+package com.knits.kncare.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+
+@Data
+public abstract class AbstractSearchableDto<T> implements Searchable<T> {
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Integer limit;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Integer page;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String sort;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Sort.Direction dir;
+
+    @Override
+    @JsonIgnore
+    public Specification<T> getSpecification() {
+        Specification<T> spec = Specification.where(null);
+        // TODO: 26/07/2021 add search by date
+        return spec;
+    }
+
+    @Override
+    @JsonIgnore
+    public Pageable getPageable() {
+        return PageRequest.of(
+                (page != null) ? page : 0,
+                (limit != null && limit >= 0) ? limit : 10,
+                getSortSpec()
+        );
+    }
+
+    @Override
+    @JsonIgnore
+    public Sort getSortSpec() {
+        if (sort == null) return Sort.unsorted();
+        return (dir != null && dir == Sort.Direction.DESC) ?
+                Sort.by(sort).descending() : Sort.by(sort).ascending();
+    }
+}
--- src/main/java/com/knits/kncare/dto/MemberDto.java
+++ src/main/java/com/knits/kncare/dto/MemberDto.java
@@ -1,22 +1,19 @@
-package com.example.KnCare.dto;
+package com.knits.kncare.dto;
 
-import com.example.KnCare.model.*;
 import lombok.*;
-
-import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class MemberDto extends DtoBase{
+public class MemberDto extends AbstractSearchableDto {
 
     private Long id;
     private String onBoardDate;
     private String offBoardDate;
 
-
+/*
     Employee employee;
 
     Set<MemberTraining> memberTrainings;
@@ -34,4 +31,6 @@ public class MemberDto extends DtoBase{
     Set<EmailTemplate> emailTemplates;
 
     Set<Notification> notifications;
+    */
+
 }
