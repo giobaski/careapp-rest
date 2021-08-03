e9e53b0b6e8873719dbb07cb67ed0c11f54132b4
midrob <midrob@ttu.ee>
2021-08-03 12:58:37 +0300

    update swagger config

--- src/main/java/com/example/KnCare/config/SwaggerConfig.java
+++ src/main/java/com/example/KnCare/config/SwaggerConfig.java
@@ -2,10 +2,13 @@ package com.example.KnCare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static com.google.common.collect.Lists.newArrayList;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -14,30 +17,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-@Import(BeanValidatorPluginsConfiguration.class)
-@EnableSwagger2
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
-    // swagger config, defines scope and paths
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(PathSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.KnCare"))
+                .paths(PathSelectors.ant("/api/v1/*"))
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponses(HttpMethod.GET, newArrayList(
+                        new ResponseBuilder().code("500")
+                                .description("500 message").build(),
+                        new ResponseBuilder().code("403")
+                                .description("Forbidden!!!!!").build()
+                ));
     }
 
-    // override of api info
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "My REST API",
-                "Simple api to find a 'person' from database.",
-                "1.0",
-                "Terms of service",
-                new Contact("Dev team", "www.company.com", "company@gmail.com"),
-                "License of API", "API license URL", Collections.emptyList());
+        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", new Contact("John Doe", "www.example.com", "myeaddress@company.com"), "License of API", "API license URL", Collections.emptyList());
+        return apiInfo;
     }
 }
