58651cf93fab95610af176d41994e246ad932c5c
midrob <midrob@ttu.ee>
2021-08-08 19:43:32 +0300

    add criteria api query to members

--- src/main/java/com/knits/kncare/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knits.kncare.repository;
-
-import com.knits.kncare.model.ems.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
