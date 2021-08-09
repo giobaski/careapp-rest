18f3d3a8885f294f15a03d302e376c3766b13629
midrob <midrob@ttu.ee>
2021-08-09 13:36:16 +0300

    test ems mocked api - employees

--- src/main/java/com/knits/kncare/dto/MemberSearch.java
+++ src/main/java/com/knits/kncare/dto/MemberSearch.java
@@ -25,5 +25,4 @@ public class MemberSearch {
 
     private String workingPosition;
 
-
 }
--- src/test/java/com/knits/kncare/controller/RestEmployeeTest.java
+++ src/test/java/com/knits/kncare/controller/RestEmployeeTest.java
@@ -1,26 +1,53 @@
 package com.knits.kncare.controller;
 
+import com.knits.kncare.dto.EmployeePage;
+import com.knits.kncare.service.EmployeeService;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+//@ExtendWith(SpringExtension.class)
+//@WebFluxTest(controllers = EmployeeController.class)
+//@Import(EmployeeService.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class EmployeeControllerTest {
+public class RestEmployeeTest {
+
+//    @Autowired
+//    private WebTestClient webClient;
+
     @BeforeAll
     public static void setup() {
         baseURI = "http://localhost:8080";
         port = 8080;
     }
 
+//    @Test
+//    public void getEmployeesFromEmc(){
+//        webClient
+//                .get()
+//                .uri("http://localhost:9000/employees")
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectBodyList(EmployeePage.class);
+//    }
+
     @Test
     public void givenUrl_whenSuccessOnGetsResponse_thenCorrect() {
         get("/api/v1/employees").then().statusCode(200)
                 .assertThat().body("size()", greaterThan(0));
     }
+
+
     @Test
     public void givenUrl_whenSuccessOnPostResponse_thenCorrect() {
 
