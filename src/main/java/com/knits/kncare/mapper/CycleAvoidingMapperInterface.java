0ea174ae81a32e9f603a7fc4195c823dc8c1e15f
Marek <malahk@ttu.ee>
2021-08-23 15:24:36 +0300

    Removed redundant annotations. Renamed named mapper methods

--- src/main/java/com/knits/kncare/mapper/CycleAvoidingMapperInterface.java
+++ src/main/java/com/knits/kncare/mapper/CycleAvoidingMapperInterface.java
@@ -1,7 +1,6 @@
 package com.knits.kncare.mapper;
 
 import org.mapstruct.Context;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -10,30 +9,26 @@ import java.util.Set;
 public interface CycleAvoidingMapperInterface<MDL, DTO> extends MapperInterface<MDL, DTO> {
 
     @Override
-    @Named("abc3")
+    @Named("toDtoNewCyclic")
     default DTO toDto(MDL model) {
         return toDtoCyclic(model, new CycleAvoidingMappingContext());
     }
 
     @Override
-    @Named("abc1")
+    @Named("toDtoListNewCyclic")
     default List<DTO> toDtoList(List<MDL> modelList) {
         return toDtoListCyclic(modelList, new CycleAvoidingMappingContext());
     }
 
     @Override
-    @Named("abc")
+    @Named("toDtoSetNewCyclic")
     default Set<DTO> toDtoSet(Set<MDL> modelSet) {
         return toDtoSetCyclic(modelSet, new CycleAvoidingMappingContext());
     }
 
-
-    @IterableMapping(qualifiedByName = {"toDtoCyclic"})
     DTO toDtoCyclic(MDL model, @Context CycleAvoidingMappingContext context);
 
-    //    @IterableMapping(qualifiedByName = {"toDtoCyclic", "toDtoListCyclicInter", "toDtoSetCyclicInter"})
     Set<DTO> toDtoSetCyclic(Set<MDL> modelSet, @Context CycleAvoidingMappingContext context);
 
-    //    @IterableMapping(qualifiedByName = {"toDtoCyclic", "toDtoListCyclicInter", "toDtoSetCyclicInter"})
     List<DTO> toDtoListCyclic(List<MDL> dtoList, @Context CycleAvoidingMappingContext context);
 }
